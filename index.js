12efebff4503a35722cd1225c27326ad7af8da50
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-27 13:21:37 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(word => word.toLowerCase() === string.toLowerCase());
+};
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(word => word.toLowerCase().indexOf(string.toLowerCase()) === 0);
+};
+
+function matchName(drivers, string){
+    return drivers.filter(word => word.name === string)
+}
